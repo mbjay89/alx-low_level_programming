69fa16bcd88e894e0f766eec1fd102f7c90f0817
mbjay89 <mbsiraj.89@hotmail.com>
2022-10-13 17:42:56 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
-int main () {
+int main(void)
+{
 	   char string[55];
 
-	      strcpy(string, "\"Programming is like building a multilingual puzzle");
-	         puts(string);
-		    
-		    return(0);
+	   strcpy(string, "\"Programming is like building a multilingual puzzle");
+	   puts(string);
+	return (0);
 }
