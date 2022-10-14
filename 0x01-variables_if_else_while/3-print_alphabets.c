3717a936dfde8a1d8b193a7d590cc5f7b309dec4
mbjay89 <mbsiraj.89@hotmail.com>
2022-10-14 14:27:41 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *  *main - Entry point
+ *  *
+ *  *Return: Always 0 (Success)
+ *
+ *  * *2. Alphabets using putchar()
+ *  *
+ */
+int main(void)
+{
+	int i;
+	char stringSmall[26] = {"abcdefghijklmnopqrstuvwxyz"};
+	char stringCapital[26] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+
+	for (i = 0; i < 26; i++)
+	putchar(stringSmall[i]);
+	for (i = 0; i < 26; i++)
+	putchar(stringCapital[i]);
+	putchar('\n');
+	return (0);
+}
