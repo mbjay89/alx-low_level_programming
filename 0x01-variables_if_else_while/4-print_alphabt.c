ee38a2fe817268c35b180ba5c8e00e741098e4b5
mbjay89 <mbsiraj.89@hotmail.com>
2022-10-14 14:59:05 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,10 +12,10 @@ int main(void)
 	char string;
 
 	for (string = 'a'; string <= 'z'; string++)
-		{
-			if (string != 'q' && string != 'e')
-			putchar(string);
-		}
+	{
+		if (string != 'q' && string != 'e')
+		putchar(string);
+	}
 	putchar('\n');
 	return (0);
 }
