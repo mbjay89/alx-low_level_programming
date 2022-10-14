31fddcba8f4e8494e6e8e0169922dca089d8e5a1
mbjay89 <mbsiraj.89@hotmail.com>
2022-10-14 13:43:12 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *  *main - Entry point
+ *  *
+ *  *Return: Always 0 (Success)
+ *
+ *  * *2. Alphabets using putchar()
+ *  *
+ */
+int main(void)
+{
+	int i;
+	char string[26] = {"abcdefghijklmnopqrstuvwxyz"};
+	for (i = 0; i < 26; i++)
+	putchar(string[i]);
+	putchar('\n');
+	return (0);
+}
