928dbc2084a9057fd3ff92ad9730453410f45650
mbjay89 <mbsiraj.89@hotmail.com>
2022-10-15 10:11:32 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,10 +10,10 @@
 int main(void)
 {
 	int i;
-	char arr[58] = {"and that piece of art is useful\" - Dora Korpar, 2015-10-19"};
+	char s[58] = {"and that piece of art is useful\" - Dora Korpar, 2015-10-19"};
 
 	for (i = 0; i < 58; i++)
-	putchar(arr[i]);
+	putchar(s[i]);
 	putchar('\n');
 	return (1);
 }
