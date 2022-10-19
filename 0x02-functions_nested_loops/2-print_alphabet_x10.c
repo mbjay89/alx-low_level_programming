2486ba8f2ce00734f0b7bd74047a6dbd878a297c
mbjay89 <mbsiraj.89@hotmail.com>
2022-10-18 17:32:46 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,10 @@
 */
 void print_alphabet_x10(void)
 {
-	int i;
+	int i,x;
 	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
-	
-	for (i = 0; i < 10; i++)
+
+	for (x = 0; x < 10; x++)
 	{
 	for (i = 0; i < 26; i++)
 		_putchar(alph[i]);
