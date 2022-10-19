79866eb57642a6ec1b733799bee4f82862f33507
mbjay89 <mbsiraj.89@hotmail.com>
2022-10-18 17:02:26 -0700

    Task 0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  *_putchar -Print _putchar
+ *  *
+ *  *Return: 0 (Success)
+ *
+ *  * *0. _putchar
+ *  *
+ */
+int main(void)
+{
+	int i;
+	char ch[8];
+
+	for (i = 0; i < 8; i++)
+	_putchar(ch[i]);
+
+	return (0);
+}
