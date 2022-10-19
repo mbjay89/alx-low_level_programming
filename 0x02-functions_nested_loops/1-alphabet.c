bcb6a2441868fb6fa5ee1f4a643472a4205c5680
mbjay89 <mbsiraj.89@hotmail.com>
2022-10-18 17:23:58 -0700

    Task 1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- *  *  * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ *  *  * print_alphabet - prints the alphabet, in lowercase, and a new line
  *    *    *
  *     *     *No return values
 */
- void print_alphabet(void)
+void print_alphabet(void)
 {
 	int i;
 	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
 
-	for (i = 0; i< 26; i++)
+	for (i = 0; i < 26; i++)
 		_putchar(alph[i]);
 	_putchar('\n');
 }
