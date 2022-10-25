cf7e71a75fc2c8de9c4deac70cd1381439d0d109
mbjay89 <mbsiraj.89@hotmail.com>
2022-10-25 14:47:20 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,9 +7,10 @@
 */
 void puts_half(char *str)
 {
-	int i = 0, l = _strlen(str);
+	int i = 0, l = 0;
 /*	int n = (l - 1) / 2; */
-
+	while (str[l] != '\0')
+		l++;
 	for (; i <= l ; i++)
 	{
 		if (i > l / 2 /* && l % 2 == 0*/)
