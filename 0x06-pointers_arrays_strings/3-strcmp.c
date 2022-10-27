84738855dfd9781736b9c18851a27d7fd29c17d1
mbjay89 <mbsiraj.89@hotmail.com>
2022-10-27 13:45:57 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *  *_strcmp - function that compares two strings.
+ *  *@s1 : input first integer
+ *  *@s2 : input second integer
+ *  * Return: pointer to dest
+*/
+int _strcmp(char *s1, char *s2)
+{
+	return (*s1 - *s2);
+}
