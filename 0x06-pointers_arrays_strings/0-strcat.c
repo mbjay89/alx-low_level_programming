b06e994bd49a217148414a51292d82c77111a268
mbjay89 <mbsiraj.89@hotmail.com>
2022-10-27 10:39:50 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,5 @@
 #include "main.h"
 /**
-  *  * _strlen - a function  that returns the length of a string.
-  *  *@s : input pointer to a string
-  *  * Return: the length of string
-  */
-int _strlen(char *s)
-{
-	int x = 0;
-
-	while (*s != '\0')
-	{
-		x++;
-		s++;
-	}
-	return (x);
-}
-/**
  *  **_strcat - function that concatenates two strings.
  *  *@dest : input pointer to a char
  *  *@src : input pointer to a cahr
@@ -27,7 +11,7 @@ char *_strcat(char *dest, char *src)
 
 	while (*dest)
 		dest++;
-	while(*src)
+	while (*src)
 		*dest++ = *src++;
 	*dest = '\0';
 
