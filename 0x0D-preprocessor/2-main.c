77fd7ea00eb0a2d45256ce13a7004341721315e7
mbjay89 <mbsiraj.89@hotmail.com>
2022-11-15 12:58:31 -0800

    Task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,11 +3,10 @@
 /**
  * main - a program that prints the name of the file it was compiled from,
  * followed by a new line.
- * Return : Always 0 (Success)
+ * Return: Always 0 (Success)
 */
 int main(void)
 {
 	printf("%s\n", __FILE__);
-	
 	return (0);
 }
